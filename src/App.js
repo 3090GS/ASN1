88ce114bde10d30953bb24b20a03a6bce9106bf0
3090gs <dimetrius.blade@gmail.com>
2022-08-31 01:11:46 +0300

    oleg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ASN1 from "@lapo/asn1js";
+import Hex from "@lapo/asn1js/hex";
 
-function App() {
+const App = () => {
+  const names = [
+    { id: 1, name: "іванов іван іванович" },
+    { id: 2, name: "олеговський олег олегович" },
+    { id: 3, name: "костянтинів костянтин костянтинович" },
+  ];
+  const [isInputOpen, setInputOpen] = useState(false);
+  const areaChanger = () => {
+    setInputOpen(!isInputOpen);
+  };
+
+  const [drag, setDrag] = useState(false);
+  const dragStartHandler = (e) => {
+    e.preventDefault();
+    setDrag(true);
+  };
+  const dragLeaveHandler = (e) => {
+    e.preventDefault();
+    setDrag(false);
+  };
+  const dropHandler = (e) => {
+    e.preventDefault();
+    let files = [...e.dataTransfer.files];
+    decoder(files[0]);
+  };
+
+  const decoder = (e, stream, size) => {
+    const Human = ASN1.decode("Human", function () {
+      this.seq().obj(
+        this.key("commonName").octstr(),
+        this.key("organizationName").octstr(),
+        this.key("serialNumber").octstr(),
+        this.key("countryName").octstr()
+      );
+    });
+    console.log(Human);
+    var output = Human.stream.pos(e, "der");
+
+    console.log(output, 'olegovisch');
+
+    const result = Human.decode(e, "per");
+    console.log(result, "result");
+
+    if (result.typeName() !== "SEQUENCE") {
+      throw "Неправильна структура конверта сертифіката (очікується SEQUENCE)";
+    }
+    const tbsCertificate = result.sub[0];
+    console.log(tbsCertificate, "серт");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="namesWrapper">
+        <div className="names">
+          {names.map((n) => (
+            <div className="name">{n.name}</div>
+          ))}
+        </div>
+        <button onClick={() => areaChanger()}>
+          {isInputOpen ? "Скасувати" : "Додати"}
+        </button>
+      </div>
+      <div className="content">
+        {isInputOpen ? (
+          <div className="drag_area">
+            {drag ? (
+              <div
+                className="drag"
+                onDragStart={(e) => dragStartHandler(e)}
+                onDragLeave={(e) => dragLeaveHandler(e)}
+                onDragOver={(e) => dragStartHandler(e)}
+                onDrop={(e) => dropHandler(e)}
+              >
+                Відпустіть для завантаження
+              </div>
+            ) : (
+              <div
+                className="no_drag"
+                onDragStart={(e) => dragStartHandler(e)}
+                onDragLeave={(e) => dragLeaveHandler(e)}
+                onDragOver={(e) => dragStartHandler(e)}
+              >
+                Перетягніть для завантаження
+              </div>
+            )}
+          </div>
+        ) : (
+          <div>ОЦЕ ТУТ ІНФО</div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
